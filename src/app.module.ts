cca90f5eab88eef6ef533ff57b30e5cc09b1ab42
Henrique Melo <hacmelo@gmail.com>
2023-10-19 22:42:53 -0300

    (#129) corrige nome healthCheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controler';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './config/db/db.module';
 import { DbService } from './config/db/db.service';
