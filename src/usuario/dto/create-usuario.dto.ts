896d1b48cca91dddd0446f50ffbb7d2f858b52ec
Henrique Melo <hacmelo@gmail.com>
2023-10-19 22:40:45 -0300

    (#129) adiciona entidade e dtos usuario

---
+++ src/usuario/dto/create-usuario.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUsuarioDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(60)
+  @MinLength(5)
+  nome!: string;
+
+  @IsOptional()
+  @IsString()
+  foto?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(100)
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  senha!: string;
+
+  @IsOptional()
+  @IsBoolean()
+  admin?: boolean;
+}
