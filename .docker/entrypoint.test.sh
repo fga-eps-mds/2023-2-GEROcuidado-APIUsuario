f86f0ade3480ac0596ecc61b456b26fbeca9499e
Henrique Melo <hacmelo@gmail.com>
2023-10-21 21:41:27 -0300

    (#143) reverte mudancas
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>

--- .docker/entrypoint.test.sh
+++ .docker/entrypoint.test.sh
@@ -13,9 +13,13 @@ npm run typeorm:run
 if ! [ -z $TEST ] && [ $TEST == 'test' ]; then
     echo "Running unit tests"
     npm run test
-
-    echo "Running E2E tests"
+elif ! [ -z $TEST ] && [ $TEST == 'e2e' ]; then
+    echo "Running e2e tests"
     npm run test:e2e
+elif ! [ -z $TEST ] && [ $TEST == 'lint' ]; then
+    echo "Running lint"
+    npm run lint
+    npm run format
 else
     echo "Running test"
     npm run start:dev
